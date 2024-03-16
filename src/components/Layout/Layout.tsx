c0b0881f5b6570cc21b6865cd2ee6c84b7df2e48
doolotkeldi <dolotalanov35@gmail.com>
2024-03-17 00:37:13 +0600

    odejda-section

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -3,12 +3,14 @@ import Header from '../Header/Header'
 import Footer from '../Footer/Footer'
 import { Outlet } from 'react-router-dom'
 import SectionONe from '../SectionOne/SectionONe'
+import Odejda from '../Odejda/Odejda'
 
 function Layout() {
   return (
     <div>
         <Header/>
         <SectionONe/>
+        <Odejda/>
         <Outlet/>
         <Footer/>
 
