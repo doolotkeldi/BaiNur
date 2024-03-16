9bac53cf5b72ac2aa49665b64edbf0921db37d2e
doolotkeldi <dolotalanov35@gmail.com>
2024-03-16 11:07:46 +0600

    assets

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import User from './svg/user.svg'
+import Basket from './svg/basket.svg'
+import Vektor from './svg/Vector.svg'
+import Favorites1 from './svg/favorites1.svg'
 
 function Header() {
   return (
     <div>
 
-    <Link to='/'>Home</Link>
-    <Link to='/contact'>Contact</Link>
+   <li><Link to='/'>Home</Link></li> 
+   <li><Link to='/contact'>Contact</Link></li>
+    
+
     </div>
   )
 }
