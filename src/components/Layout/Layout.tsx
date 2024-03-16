1a0d836a5d73edb511673313ff9e1fd5a82c5141
doolotkeldi <dolotalanov35@gmail.com>
2024-03-16 13:14:56 +0600

    header

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -2,11 +2,13 @@ import React from 'react'
 import Header from '../Header/Header'
 import Footer from '../Footer/Footer'
 import { Outlet } from 'react-router-dom'
+import SectionONe from '../SectionOne/SectionONe'
 
 function Layout() {
   return (
     <div>
         <Header/>
+        <SectionONe/>
         <Outlet/>
         <Footer/>
 
