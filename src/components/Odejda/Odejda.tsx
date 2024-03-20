95168ccf4e7d2e0b3b650068b9a7c3a9c0f73ecc
doolotkeldi <dolotalanov35@gmail.com>
2024-03-21 00:33:53 +0600

    uraa

--- src/components/Odejda/Odejda.tsx
+++ src/components/Odejda/Odejda.tsx
@@ -7,18 +7,6 @@ import a from '../../assets/svg/a.svg';
 import b from '../../assets/svg/b.svg';
 import { Transition } from 'react-transition-group';
 
-
-
-
-
-import React, { useState } from 'react';
-import Woman from '../../assets/svg/Rectangle 5.svg';
-import './Odejda.css';
-import heart from '../../assets/svg/favorites1.svg';
-import a from '../../assets/svg/a.svg';
-import b from '../../assets/svg/b.svg';
-import { Transition } from 'react-transition-group';
-
 function Odejda() {
     const [btnImg, setBtnImg] = useState(a);
     const [filterImage, setFilterImage] = useState(false);
@@ -94,80 +82,6 @@ function Odejda() {
                 </div>
 
                 <div className="image-container container">
-    const [btnImg, setBtnImg] = useState(a);
-    const [filterImage, setFilterImage] = useState(false);
-
-    const positionclick = () => {
-        setBtnImg(btnImg === a ? b : a);
-        setFilterImage(!filterImage)
-    };
-
-    return (
-        <section>
-            <div className="container odejda-content">
-            <button onClick={positionclick}>Фильт <img className='button-image' src={btnImg} alt="" /></button>
-
-              
-                <Transition in={filterImage} timeout={1000} mountOnEnter unmountOnExit>
-                    {state => (
-                        <div className={`filter-position ${state}`}>
-                            <div className="filter-header">
-                                <div className="filter-position-content">
-                                    <h1>Мужские</h1>
-                                    <div>
-                                        <p>Футболки</p>
-                                        <p>Худи</p>
-                                        <p>Брюки</p>
-                                        <p>Рубашка</p>
-                                        <p>Бутсы</p>
-                                        <p>Кеды</p>
-                                        <p>Шорты</p>
-                                    </div>
-                                </div>
-                                <div className="filter-position-content">
-                                    <h1>Женские</h1>
-                                    <div>
-                                        <p>Футболки</p>
-                                        <p>Худи</p>
-                                        <p>Кеды</p>
-                                        <p>Рубашка</p>
-                                        <p>Брюки</p>
-                                        <p>Юбки</p>
-                                        <p>Свинтшоты</p>
-                                    </div>
-                                </div>
-                                <div className="filter-position-content">
-                                    <h1>Дети</h1>
-                                    <div>
-                                        <p>Футболки</p>
-                                        <p>Худи</p>
-                                        <p>Кеды</p>
-                                        <p>Рубашка</p>
-                                        <p>Юбки</p>
-                                        <p>Свинтшоты</p>
-                                        <p>Брюки</p>
-                                        <p>Шорты</p>
-                                        <p>Бутсы</p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    )}
-                </Transition>
-                
-
-                <div className="text-info">
-                    <span style={{ display: "flex" }}>
-                        <h1 style={{ color: "#0D99FF" }}>Главная // </h1>
-                        <h1>Все товары </h1>
-                    </span>
-                    <span style={{ display: "flex" }}>
-                        <h1>Главная // </h1>
-                        <h1 style={{ color: "#0D99FF" }}>Все товары </h1>
-                    </span>
-                </div>
-
-                <div className="image-container container">
 
 <div className="image-block">
     <div className="image-heart">
@@ -306,14 +220,11 @@ function Odejda() {
 </div>
 </div>
             </div>
-        </section>
-    );
-</div>
-</div>
             </div>
         </section>
-    );
+    )
+
 }
 
 export default Odejda;
-export default Odejda;
+
